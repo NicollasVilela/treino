eef1efe649d9a3ff97758ab233dae059e46b9187
NicollasVilela <162446919+NicollasVilela@users.noreply.github.com>
2024-03-07 00:36:06 +0000

    treino

---
+++ script.js
@@ -0,0 +1,77 @@
+
+//1- 
+
+// var INDICE = 13
+// var SOMA= 0
+// var K = 0
+
+// while(K < INDICE){
+//   K = K + 1;
+//   SOMA = SOMA + K;
+//   document.write(`<br>Valor final: ${SOMA}`)
+// }
+
+//valor final = 91
+
+// ----------------------------------------------------------------------
+
+//2- 
+
+// function fibonacci(termo){
+//   var ultimo = 1, penultimo = 0, num;
+
+//   if(termo <= 2){
+//     return termo - 1
+//   }
+//   else{
+//     for(var i = 3; i <= termo; i++){
+//       num = ultimo + penultimo
+//       penultimo = ultimo 
+//       ultimo = num
+//     }
+//     return num
+//   }
+// }
+
+// var termoDesejado = 8
+// var resultado = fibonacci(termoDesejado)
+
+// document.write(`<br>O termo `, termoDesejado, ` da sequencia de Fibonacci é `, resultado)
+
+// ----------------------------------------------------------------------
+
+//3- 
+// a) 9
+// b) 128
+// c) 81
+// d) 100
+// e) 13
+// f) ?
+
+// ----------------------------------------------------------------------
+
+//4-
+
+// sao 3 interruptores, tem que ligar o primeiro e esperar um tempo ate a lampada esquentar, em seguida ligar o segundo interruptor e ir rapido pra sala. ao chegar la vai ter uma lampada acesa (2 interruptor), uma lampada que vai estar quente pos foi desligada a pouco tempo (1 interruptor) e a lampada que nao estiver acesa nem quente é o 3 interruptor
+
+// ----------------------------------------------------------------------
+
+//5-
+
+// function inverterString(str){
+//   var res = " "
+
+//   for(var i = str.length - 1; i >= 0; i--){
+//     res += str[i]
+//   }
+//   return res
+// }
+
+// var string1 = 'Gupy'
+// var stringInv = inverterString(string1)
+
+// document.write(`<br> ${stringInv}`)
+
+
+
+  
